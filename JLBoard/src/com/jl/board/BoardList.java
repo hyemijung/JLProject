948e3a1332709ce3519c6327310761686cbf8b9e
hyemijung84 <hyemijung84@gmail.com>
2019-03-20 11:39:47 +0900

    JLBoard

---
+++ JLBoard/src/com/jl/board/BoardList.java
@@ -0,0 +1,9 @@
+package com.jl.board;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(value="/board/list")
+public class BoardList extends HttpServlet{
+
+}
---
+++ JLBoard/src/com/jl/member/MemberList.java
@@ -0,0 +1,13 @@
+package com.jl.member;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(value="/member/list")
+public class MemberList extends HttpServlet {
+
+	
+	
+	
+	
+}
